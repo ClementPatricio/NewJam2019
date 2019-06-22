9fc73458d4776efb17064f28dcc30dc7e756e420
ClementPatricio <genma23@hotmail.fr>
2019-06-22 18:26:24 +0200

    Ajout packages
    ajout post processing et input manager

--- NewJam2019/Assets/Scripts/ObjectToHandScript.cs
+++ NewJam2019/Assets/Scripts/ObjectToHandScript.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class ObjectToHandScript : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject rightHand;
+    [SerializeField]
+    private GameObject leftHand;
+
     private void SnapObjectToRightHand()
     {
         
