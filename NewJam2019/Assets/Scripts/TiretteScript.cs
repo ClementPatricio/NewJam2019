58f4324157e10f19ed4f08c244e715f932f3ebab
Le6barbare <Le6barbare@gmail.com>
2019-06-24 00:25:10 +0200

    Animation Tirette

--- NewJam2019/Assets/Scripts/TiretteScript.cs
+++ NewJam2019/Assets/Scripts/TiretteScript.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class TiretteScript : MonoBehaviour
 {
+    private Animator anim;
+
+    void Start() {
+        anim = GetComponent<Animator>();
+    }
 
     private void OnMouseDown()
     {
         GameManager.gameManager.light.enabled = !GameManager.gameManager.light.enabled;
-        this.GetComponent<AudioSource>().Play();
+        //this.GetComponent<AudioSource>().Play();
+        anim.Play("Tirette", 0, 0.25f);
     }
 }
